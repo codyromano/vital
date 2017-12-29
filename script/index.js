1da972335578910964f42a94baba25aad44e521e
Cody Romano <codyromano@gmail.com>
2017-12-29 17:36:59 -0500

    Configure Music route

--- script/index.js
+++ script/index.js
@@ -3,10 +3,12 @@ import PropTypes from 'prop-types';
 import { render } from 'react-dom';
 import { HashRouter, Switch, Route } from 'react-router-dom';
 import App from 'vital-components/App';
+import ConfigureMusicPage from 'vital-components/ConfigureMusicPage';
 
 const AppWithRouter = () => (
   <HashRouter>
     <Switch>
+      <Route path="/configure-music" exact={true} component={ConfigureMusicPage} />
       <Route path="/" component={App} />
     </Switch>
   </HashRouter>
