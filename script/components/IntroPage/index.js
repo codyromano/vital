f20b17dcdee18a2956984ccfea447012e28e7fd6
Cody Romano <codyromano@gmail.com>
2018-01-15 21:04:05 -0800

    Intro page

--- script/components/IntroPage/index.js
+++ script/components/IntroPage/index.js
@@ -2,21 +2,43 @@ import React from 'react';
 import BasePage, { PageWidthContainer } from 'vital-components/BasePage';
 import ActionButton from 'vital-components/ActionButton';
 import { Link } from 'react-router-dom';
+import './IntroPage.scss';
 
 export default class IntroPage extends React.Component {
+  componentDidMount() {
+    this.vid.play();
+  }
   render() {
     return (
-      <BasePage>
-        <PageWidthContainer>
-          <p>DataBass increases the intensity of your workout music 
-            as you're running or biking.</p>
+      <main className="intro-page full-screen">
+      <video
+        className="run-video full-screen"
+        ref={(vid) => {this.vid = vid; }}
+        controls={false}
+        loop={true}
+        autoplay={true}>
+        <source
+          autoplay={true}
+          width={'100%'}
+          src="https://giant.gfycat.com/SadColorlessFlicker.webm"
+          type="video/webm"/>
+      </video>
 
-          <p>Go fast to increase the tempo and bass. If you slow
-            down, so will your music.</p>
+        <div className="intro-content full-screen">
+          <PageWidthContainer>
+            <h1 class="logo">DataBass</h1>
 
-          <ActionButton to="/configure-music">Choose a song</ActionButton>
-        </PageWidthContainer>
-      </BasePage>
+            <p>DataBass increases the intensity of your workout
+            music according to your performance.</p>
+
+            <p>The faster you run, the stronger the beats.</p>
+
+            <div className="cta-wrapper">
+              <ActionButton to="/configure-music">Choose a song</ActionButton>
+            </div>
+          </PageWidthContainer>
+        </div>
+      </main>
     );
   }
 }
